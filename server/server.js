8634553c15cdcfe940c3b297df4ff16ea21dddbb
marine727 <47843216+marine727@users.noreply.github.com>
2020-04-20 08:28:53 +0430

    impliment socket io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 8080;
 var app = express();
+var server = http.createServer(app);
+var io = socketIo(server);
+
+io.on('connection' , (socket)=>{
+    console.log('New User Connected..');
+    socket.on('disconnect' , ()=>{
+        console.log("user desconnected")
+    })
+})
+
+
 
 app.use(express.static(publicPath))
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`server run on port ${port}.`);
 })
